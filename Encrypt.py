8aba88e542a96dbcbb9fe712324a76c74c612555
Lxs_ <112419013+Lxs06@users.noreply.github.com>
2024-03-28 12:13:21 -0400

    update

--- Encrypt.py
+++ Encrypt.py
@@ -39,25 +39,25 @@ enc_dict = {
     "8":"i",#used
     "9":"a",#used
 }
-String file_name = input("Enter file name: ")
+
+file_name = input("Enter file name: ")
 
 file = open(file_name+".txt","r")
-new_file open(file_name+"_NSLE.txt","w")
-
-words = file.split(' ')
-
-for word in words:
-    if word != "\n":
-        for char in word:
-            try:
-                new_file.write(enc_dict[char])
-            except:
-                print("Error while encryption...")
-                os.remove(file_name+"_NSLE.txt")
-                time.sleep(5)
-                exit()
-    else:
-        new_file.write("\n")
-
+new_file = open(file_name+"_NSLE.txt","w")
+lines = file.readlines()
+for line in lines:
+    words = line.split(' ')
+    for word in words:
+        if word != "\n":
+            for char in word:
+                try:
+                    new_file.write(enc_dict[char])
+                except:
+                    print("Error while encryption...")
+                    os.remove(file_name+"_NSLE.txt")
+                    time.sleep(5)
+                    exit()
+        else:
+            new_file.write("\n")
 new_file.close()
 file.close()
